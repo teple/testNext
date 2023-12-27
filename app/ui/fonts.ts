d198a8079cb2f184f2813a68746b94279859766e
yozzzo <yz.tomoda@gmail.com>
2023-12-28 02:19:12 +0900

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Rubik_Marker_Hatch } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const rubik_marker_hatch = Rubik_Marker_Hatch({
+    weight: '400',
+    subsets: ['latin'],
+  });
