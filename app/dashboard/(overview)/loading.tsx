ce9cb287016ad9c52fc4928dcfb1365a3c66375b
yozzzo <yz.tomoda@gmail.com>
2024-01-22 19:41:23 +0900

    commit

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+import { Entity, Scene } from 'aframe-react';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
---
+++ app/dashboard/(overview)/page2.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Entity, Scene } from 'aframe-react';
+
+// const ARPage = () => {
+//   if (typeof window !== "undefined") {
+//     require("aframe");
+//     setRendered(true);
+//   }
+
+//   return (
+//     <Scene>
+//       <Entity camera="active: true" />
+//       <Entity
+//         primitive="a-marker"
+//         type="pattern"
+//         url="path/to/pattern-marker.patt"
+//       >
+//         <Entity
+//           primitive="a-image"
+//           src="path/to/image.png"
+//           position="0 0 0"
+//         />
+//       </Entity>
+//     </Scene>
+//   );
+// };
